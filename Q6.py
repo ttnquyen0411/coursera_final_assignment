1257623660c427b6a3b081ff1c2ed77994a6b49d
ttnquyen0411 <115357072+ttnquyen0411@users.noreply.github.com>
2022-11-13 13:23:53 +0700

    Add files via upload

---
+++ Q6.py
@@ -0,0 +1,39 @@
+import yfinance as yf
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+
+def make_graph(stock_data, revenue_data, stock):
+    fig = make_subplots(rows=2, cols=1, shared_xaxes=True, subplot_titles=("Historical Share Price", "Historical Revenue"), vertical_spacing = .3)
+    fig.add_trace(go.Scatter(x=pd.to_datetime(stock_data.Date, infer_datetime_format=True), y=stock_data.Close.astype("float"), name="Share Price"), row=1, col=1)
+    fig.add_trace(go.Scatter(x=pd.to_datetime(revenue_data.Date, infer_datetime_format=True), y=revenue_data.Revenue.astype("float"), name="Revenue"), row=2, col=1)
+    fig.update_xaxes(title_text="Date", row=1, col=1)
+    fig.update_xaxes(title_text="Date", row=2, col=1)
+    fig.update_yaxes(title_text="Price ($US)", row=1, col=1)
+    fig.update_yaxes(title_text="Revenue ($US Millions)", row=2, col=1)
+    fig.update_layout(showlegend=False,
+    height=900,
+    title=stock,
+    xaxis_rangeslider_visible=True)
+    fig.show()
+
+#tesla_data
+gme = yf.Ticker('GME')
+gme_data = gme.history(period = "max")
+#tesla_revenue
+url = "https://www.macrotrends.net/stocks/charts/GME/gamestop/revenue"
+html_data = requests.get(url).text
+soup = BeautifulSoup(html_data, "html.parser")
+soup.find_all('title')
+gme_revenue = pd.DataFrame(columns=['Date', 'Revenue'])
+
+for row in soup.find_all("tbody")[1].find_all("tr"):
+    col = row.find_all("td")
+    date = col[0].text
+    revenue = col[1].text.replace("$", "").replace(",", "")
+
+    gme_revenue = gme_revenue.append({"Date": date, "Revenue": revenue}, ignore_index=True)
+
+make_graph(gme_data, gme_revenue, 'Tesla')
