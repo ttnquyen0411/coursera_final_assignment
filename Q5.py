f36d8aa845026633872e4b8b38e49d0058f76b83
ttnquyen0411 <115357072+ttnquyen0411@users.noreply.github.com>
2022-11-13 14:17:16 +0700

    Update Q5.py

--- Q5.py
+++ Q5.py
@@ -1,39 +1,42 @@
-import yfinance as yf
-import pandas as pd
-import requests
-from bs4 import BeautifulSoup
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
+import yfinance as yf
+import pandas as pd
+
+stock_data = yf.download("TSLA", start="2020-01-01", end="2021-09-30", progress=False)
+revenue_data = yf.download("TSLA", start="2020-01-01", end="2021-09-30", progress=False)
+stock_data.reset_index(inplace=True)
+revenue_data.reset_index(inplace=True)
+
 
 def make_graph(stock_data, revenue_data, stock):
-    fig = make_subplots(rows=2, cols=1, shared_xaxes=True, subplot_titles=("Historical Share Price", "Historical Revenue"), vertical_spacing = .3)
-    fig.add_trace(go.Scatter(x=pd.to_datetime(stock_data.Date, infer_datetime_format=True), y=stock_data.Close.astype("float"), name="Share Price"), row=1, col=1)
-    fig.add_trace(go.Scatter(x=pd.to_datetime(revenue_data.Date, infer_datetime_format=True), y=revenue_data.Revenue.astype("float"), name="Revenue"), row=2, col=1)
+    fig = make_subplots(rows=2, cols=1,
+                        shared_xaxes=True,
+                        subplot_titles=("Historical Share Price", "Historical Revenue"),
+                        vertical_spacing=.3)
+
+    stock_data_specific = stock_data[stock_data.Date <= '2021-06-14']
+    revenue_data_specific = revenue_data[revenue_data.Date <= '2021-04-30']
+
+    fig.add_trace(go.Scatter(
+        x=pd.to_datetime(stock_data_specific.Date, infer_datetime_format=True),
+        y=stock_data_specific.Close.astype("float"), name="Share Price"), row=1, col=1)
+
+    fig.add_trace(go.Scatter(x=pd.to_datetime(revenue_data_specific.Date, infer_datetime_format=True),
+                             y=revenue_data_specific.Volume.astype("float"),
+                             name="Volume"), row=2, col=1)
+
     fig.update_xaxes(title_text="Date", row=1, col=1)
     fig.update_xaxes(title_text="Date", row=2, col=1)
     fig.update_yaxes(title_text="Price ($US)", row=1, col=1)
     fig.update_yaxes(title_text="Revenue ($US Millions)", row=2, col=1)
+
     fig.update_layout(showlegend=False,
-    height=900,
-    title=stock,
-    xaxis_rangeslider_visible=True)
+                      height=900,
+                      title=stock,
+                      xaxis_rangeslider_visible=True)
+
     fig.show()
 
-#tesla_data
-Tesla = yf.Ticker('TSLA')
-tesla_data = Tesla.history(period = "max")
-#tesla_revenue
-url = "https://www.macrotrends.net/stocks/charts/TSLA/tesla/revenue"
-html_data = requests.get(url).text
-soup = BeautifulSoup(html_data, "html.parser")
-soup.find_all('title')
-tesla_revenue = pd.DataFrame(columns=['Date', 'Revenue'])
 
-for row in soup.find_all("tbody")[1].find_all("tr"):
-    col = row.find_all("td")
-    date = col[0].text
-    revenue = col[1].text.replace("$", "").replace(",", "")
-
-    tesla_revenue = tesla_revenue.append({"Date": date, "Revenue": revenue}, ignore_index=True)
-
-make_graph(tesla_data, tesla_revenue, 'Tesla')
+make_graph(stock_data, revenue_data, 'TSLA')
