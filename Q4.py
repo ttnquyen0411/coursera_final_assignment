8c4a2ec8be447871437526bc5f4f3f406000a9f3
ttnquyen0411 <115357072+ttnquyen0411@users.noreply.github.com>
2022-11-13 13:07:02 +0700

    Add files via upload

---
+++ Q4.py
@@ -0,0 +1,21 @@
+import yfinance as yf
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://www.macrotrends.net/stocks/charts/GME/gamestop/revenue"
+html_data = requests.get(url).text
+soup = BeautifulSoup(html_data, "html.parser")
+soup.find_all('title')
+gme_revenue = pd.DataFrame(columns=['Date', 'Revenue'])
+
+for row in soup.find_all("tbody")[1].find_all("tr"):
+    col = row.find_all("td")
+    date = col[0].text
+    revenue = col[1].text.replace("$", "").replace(",", "")
+
+    gme_revenue = gme_revenue.append({"Date": date, "Revenue": revenue}, ignore_index=True)
+
+gme_revenue.dropna(inplace=True)
+gme_revenue = gme_revenue[gme_revenue['Revenue'] != ""]
+print(gme_revenue.tail(5))
